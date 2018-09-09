32a8b5cef4fc13a320a3276ae325b6d024ad4ec9
David Duyn <duyndavid@gmail.com>
2018-09-10 00:09:29 +0200

    GetItems

--- src/api/items-service.service.ts
+++ src/api/items-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, URLSearchParams } from '@angular/http'
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +13,13 @@ export class ItemsServiceService {
   constructor(private http: Http) { }
 
     public getItems(): Observable<any> {
-      return this.http.get(this.baseURL)
+      return this.http.get(`${this.baseURL}/items`, { headers: this.getHeaders()})
+        .pipe(map(x => x.json()))
     }
 
     public getItem(id: number): Observable<any> {
-        return this.http.get(`${this.baseURL}/items/${id}`, { headers: this.getHeaders()})
-          .pipe(map(x => x.json())
+      return this.http.get(`${this.baseURL}/items/${id}`, {headers: this.getHeaders()})
+        .pipe(map(x => x.json()))
     }
 
     private getHeaders() {
--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Books } from '../Books';
-
+import { ItemsServiceService } from '../../api/items-service.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-item',
@@ -8,59 +9,24 @@ import { Books } from '../Books';
 })
 export class ListItemComponent implements OnInit {
 
-  books: Books[]
-  printBooks: Books[]
+  printBooks: Observable<Books[]>
   chkStock: boolean
 
   @Output() onSelectedItem: EventEmitter<Books>
 
-  constructor() {
+  constructor(private api: ItemsServiceService) {
     this.onSelectedItem = new EventEmitter
-    this.books = [
-      {
-        key: 1, 
-        name: "Rebelión en la Granja", 
-        description: "Rebelión en la granja es una novela satírica del escritor británico George Orwell", 
-        author: "George Orwell", 
-        prize: 19.99, 
-        currency: "€", 
-        pages: 220, 
-        stock: true,
-        photo: "https://imagessl1.casadellibro.com/a/l/t0/51/9788499890951.jpg"
-      },
-      {
-        key: 2, 
-        name: "Un Mundo Feliz", 
-        description: "Un mundo feliz es la novela más famosa del escritor británico Aldous Huxley", 
-        author: "Aldous Huxley", 
-        prize: 17.95, 
-        currency: "€", 
-        pages: 135, 
-        stock: true,
-        photo: "http://t3.gstatic.com/images?q=tbn:ANd9GcT0LvMFwN7ApsQlN6RMYc6prOXsg85jcWTa3kUwtWEU87DgwWTe"
-      },
-      {
-        key: 3, 
-        name: "El Señor de las Moscas", 
-        description: "El señor de las moscas es la primera y más célebre novela de William Golding", 
-        author: "William Golding", 
-        prize: 23.56, 
-        currency: "€", 
-        pages: 300, 
-        stock: false,
-        photo: "http://t2.gstatic.com/images?q=tbn:ANd9GcQ2JYVlrfxYWPKa1w3TtBqY30bjYY4IrMSfT2vNsCPU845A1OwY"
-      }
-    ]
-    this.printBooks = this.books
+   
+    this.api.getItems().subscribe(x => {
+      this.printBooks = x
+      console.log(this.printBooks)
+    })
+ 
+
    }
 
   ngOnInit() {
   }
-
-  /*filterStock(){
-    //if (this.chkStock) this.printBooks = this.books.filter(x => x.stock)
-    //else this.printBooks = this.books
-  }*/
 
   onSelectItem(book: Books){
     this.onSelectedItem.emit(book)
